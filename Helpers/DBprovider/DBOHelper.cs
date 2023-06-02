57198612942b50ec5172608cf3bcd263cee622b1
ben_lai <ben_lai@ekera.com.tw>
2023-06-02 13:36:55 +0800

    更新

---
+++ Helpers/DBprovider/DBOHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace DBproviderUtility
+{
+    public interface IDBTransaction
+    {
+        void Commit();
+        void RollBack();
+    }
+
+    public class DBOHelper
+    {
+        public object CreateProvider(string AssemblyName, string ProviderName)
+        {
+            return Assembly.Load(AssemblyName).CreateInstance($"{AssemblyName}.{ProviderName}");
+        }
+
+        public object CreateProvider(string AssemblyName, string ProviderName, out IDBTransaction Trans)
+        {
+            Trans = (IDBTransaction)Assembly.Load("SqlHelper").CreateInstance("BenLai.SqlUtility.DBTransaction");
+            return Assembly.Load(AssemblyName).CreateInstance($"{AssemblyName}.{ProviderName}", false, BindingFlags.Default, null, new object[] { Trans }, null, null);
+        }
+    }
+}
--- Helpers/SqlHelper/SQLHelper.cs
+++ Helpers/SqlHelper/SQLHelper.cs
@@ -4,16 +4,25 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using System.Reflection;
+using DBproviderUtility;
 
 namespace BenLai.SqlUtility
 {
     public class SqlHelper
     {
         private SqlConnection Connection { get; }
+        private DBTransaction DBTrans { get; set; }
         public SqlHelper(string ConnectionString)
         {
             Connection = new SqlConnection(ConnectionString);
         }
+        public SqlHelper(string ConnectionString, DBTransaction DBTrans)
+        {
+            Connection = new SqlConnection(ConnectionString);
+            DBTrans.Con = Connection;
+            this.DBTrans = DBTrans;
+        }
+
         public IList<T> ExecuteList<T>(string Command, SqlHelperParameters objParameters) where T : class, new()
         {     
             DataTable dt = new DataTable();
@@ -46,38 +55,6 @@ namespace BenLai.SqlUtility
             return result;
         }
 
-        public async Task<IList<T>> ExecuteListAsync<T>(string Command, SqlHelperParameters objParameters) where T : class, new()
-        {
-            DataTable dt = new DataTable();
-            using (SqlDataAdapter da = new SqlDataAdapter(Command, Connection))
-            {
-                da.SelectCommand.Parameters.AddRange(objParameters.SqlHelperParameter.ToArray());
-                await Task.Run(() => da.Fill(dt));
-                da.SelectCommand.Parameters.Clear();
-            }
-            PropertyInfo[] properties = typeof(T).GetProperties();
-            IList<T> result = new List<T>();
-            foreach (DataRow row in dt.Rows)
-            {
-                T item = new T();
-                foreach (var prop in properties)
-                {
-                    if (!dt.Columns.Contains(prop.Name))
-                    {
-                        continue;
-                    }
-                    var value = row[prop.Name];
-                    if (Convert.IsDBNull(value))
-                    {
-                        value = prop.PropertyType == typeof(string) ? string.Empty : null;
-                    }
-                    prop.SetValue(item, value);
-                }
-                result.Add(item);
-            }
-            return result;
-        }
-
         public int ExecuteNonQuery(string Command, SqlHelperParameters objParameters)
         {
             using (SqlCommand cmd = new SqlCommand(Command, Connection))
@@ -85,6 +62,11 @@ namespace BenLai.SqlUtility
                 int result;
                 cmd.Parameters.AddRange(objParameters.SqlHelperParameter.ToArray());
                 Connection.Open();
+                if(DBTrans != null)
+                {
+                    cmd.Transaction = Connection.BeginTransaction();
+                    DBTrans.Trans = cmd.Transaction;
+                }
                 try
                 {
                     result = cmd.ExecuteNonQuery();
@@ -92,37 +74,14 @@ namespace BenLai.SqlUtility
                 }
                 catch
                 {
-                    Connection.Close();
                     throw;
                 }
                 finally
                 {
-                    Connection.Close();
-                }
-                return result;
-            }
-        }
-
-        public async Task<int> ExecuteNonQueryAsync(string Command, SqlHelperParameters objParameters)
-        {
-            using (SqlCommand cmd = new SqlCommand(Command, Connection))
-            {
-                int result;
-                cmd.Parameters.AddRange(objParameters.SqlHelperParameter.ToArray());
-                Connection.Open();
-                try
-                {
-                    result = await Task.Run(() => cmd.ExecuteNonQuery());
-                    cmd.Parameters.Clear();
-                }
-                catch
-                {
-                    Connection.Close();
-                    throw;
-                }
-                finally
-                {
-                    Connection.Close();
+                    if(DBTrans == null)
+                    {
+                        Connection.Close();
+                    }
                 }
                 return result;
             }
@@ -162,6 +121,22 @@ namespace BenLai.SqlUtility
                 ParameterName = express,
                 Value = value ?? Convert.DBNull
             });
+        }
+    }
+
+    public class DBTransaction : IDBTransaction
+    {
+        internal SqlTransaction Trans { get; set; }
+        internal SqlConnection Con { get; set; }
+        public void Commit()
+        {
+            Trans.Commit();
+            Con.Close();
+        }
+        public void RollBack()
+        {
+            Trans.Rollback();
+            Con.Close();
         }
     }
 }
