405ed97339100d4d53fb6b59f22cb5403d661970
ZongBen <119837502+ZongBen@users.noreply.github.com>
2023-10-12 15:45:43 +0800

    Update Ben-Validator.js

--- JsHelper/Ben-Validator.js
+++ JsHelper/Ben-Validator.js
@@ -3,26 +3,26 @@ class BenValidator {
     constructor() { }
 
     valid = (obj) => {
-        var pass = document.querySelectorAll(obj.selector).values().every((e) => {
+        let pass = document.querySelectorAll(obj.selector).values().every((e) => {
             if (e.getAttribute('data-ben-required') === 'true' && !this.#requiredValid(e)) {
-                obj.requiredValidFail(e);
+                obj.fail(e, 'required');
                 return false;
             }
             if (!this.#typeValid(e)) {
-                obj.typeValidFail(e);
+                obj.fail(e, e.getAttribute('data-ben-typeValid'));
                 return false;
             }
             return true;
         });
         if (pass) {
-            obj.validPass();
+            obj.pass();
         }
     };
 
     #requiredValid = (e) => {
         switch (e.getAttribute('type')) {
             case 'radio':
-                var name = e.getAttribute('name');
+                let name = e.getAttribute('name');
                 if (!document.querySelector(`input[name="${name}"]:checked`)) {
                     return false;
                 }
@@ -37,28 +37,20 @@ class BenValidator {
     }
 
     #typeValid = (e) => {
+        let isvalid = true;
         switch (e.getAttribute('data-ben-typeValid')) {
             case 'email':
-                var pass = String(e.value).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-                if (!pass) {
-                    return false;
-                }
+                isvalid = String(e.value).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) != null;
                 break;
             case 'decimal':
-                var digit = e.getAttribute('data-ben-digit').split(',');
-                var pattern = digit[1] === '0' ? `^\\d{1,${digit[0]}}?$` : `^\\d{1,${digit[0]}}(\\.\\d{1,${digit[1]}})?$`;
-                var pass = String(e.value).match(pattern);
-                if (!pass) {
-                    return false;
-                }
+                let digit = e.getAttribute('data-ben-digit').split(',');
+                let pattern = digit[1] === '0' ? `^\\d{1,${digit[0]}}?$` : `^\\d{1,${digit[0]}}(\\.\\d{1,${digit[1]}})?$`;
+                isvalid = String(e.value).match(pattern) != null;
                 break;
             case 'phone':
-                var pass = String(e.value).match(/^\d{10,10}?$/);
-                if (!pass) {
-                    return false;
-                }
+                isvalid = String(e.value).match(/^\d{10,10}?$/) != null;
                 break;
         }
-        return true;
+        return isvalid;
     }
 }
