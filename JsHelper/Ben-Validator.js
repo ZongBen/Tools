0346edb3c08afc15a36a6c44d0383ccf3822fcf1
ZongBen <119837502+ZongBen@users.noreply.github.com>
2023-10-12 15:19:37 +0800

    Update Ben-Validator.js

--- JsHelper/Ben-Validator.js
+++ JsHelper/Ben-Validator.js
@@ -1,65 +1,64 @@
 class BenValidator {
+
     constructor() { }
-    /*
-    驗證input或select是否有值(checkbox除外)
-    obj = {
-        selector, //選擇器
-        pass(), //通過時執行的function
-        fail(element) //失敗時執行的function, element為當下失敗的HTML元素
-    }
-    */
-    requiredValid = (obj) => {
+
+    valid = (obj) => {
         var pass = document.querySelectorAll(obj.selector).values().every((e) => {
-            switch (e.getAttribute('type')) {
-                case 'radio':
-                    var name = e.getAttribute('name');
-                    if (!document.querySelector(`input[name="${name}"]:checked`)) {
-                        obj.fail(e);
-                        return false;
-                    }
-                    break;
-                default:
-                    if (!e.value) {
-                        obj.fail(e);
-                        return false;
-                    }
-                    break;
+            if (e.getAttribute('data-ben-required') === 'true' && !this.#requiredValid(e)) {
+                obj.requiredValidFail(e);
+                return false;
+            }
+            if (!this.#typeValid(e)) {
+                obj.typeValidFail(e);
+                return false;
             }
             return true;
         });
         if (pass) {
-            obj.pass();
+            obj.validPass();
         }
     };
 
-    typeValid = () => {
-        document.querySelectorAll('input').values().filter(e => e.getAttribute('data-ben-typeValid') && e.value).every((e) => {
-            switch (e.getAttribute('data-ben-typeValid')) {
-                case 'email':
-                    var pass = String(e.value).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-                    if (!pass) {
-                        return false;
-                    }
-                    break;
-                case 'decimal':
-                    var digit = e.getAttribute('data-ben-digit').split(',');
-                    var pattern;
-                    if (digit[1] == 0) {
-                        pattern = `^\\d{1,${digit[0]}}?$`;
-                    }
-                    else {
-                        pattern = `^\\d{1,${digit[0]}}(\\.\\d{1,${digit[1]}})?$`;
-                    }
-                    var pass = String(e.value).match(pattern);
-                    if (!pass) {
-                        return false;
-                    }
-                    break;
-                case 'text':
-                    
-                    break;
-            }
-            return true;
-        });
-    };
+    #requiredValid = (e) => {
+        switch (e.getAttribute('type')) {
+            case 'radio':
+                var name = e.getAttribute('name');
+                if (!document.querySelector(`input[name="${name}"]:checked`)) {
+                    return false;
+                }
+                break;
+            default:
+                if (!e.value) {
+                    return false;
+                }
+                break;
+        }
+        return true;
+    }
+
+    #typeValid = (e) => {
+        switch (e.getAttribute('data-ben-typeValid')) {
+            case 'email':
+                var pass = String(e.value).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+                if (!pass) {
+                    return false;
+                }
+                break;
+            case 'decimal':
+                var digit = e.getAttribute('data-ben-digit').split(',');
+                var pattern = digit[1] === '0' ? `^\\d{1,${digit[0]}}?$` : `^\\d{1,${digit[0]}}(\\.\\d{1,${digit[1]}})?$`;
+                var pass = String(e.value).match(pattern);
+                if (!pass) {
+                    return false;
+                }
+                break;
+            case 'phone':
+                var pass = String(e.value).match(/^\d{10,10}?$/);
+                if (!pass) {
+                    return false;
+                }
+                break;
+        }
+        return true;
+    }
 }
